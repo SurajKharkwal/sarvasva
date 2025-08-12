30c400a823a6a2c1a3a9dd5302b174654b18b9c1
SurajKharkwal <kharkwalsuraj13@gmail.com>
2025-08-12 23:47:15 +0530

    Edit dependencies for shadcn

--- base/next/app-routes/extra/ui/shadcn/shadcn.setup.ts
+++ base/next/app-routes/extra/ui/shadcn/shadcn.setup.ts
@@ -3,15 +3,18 @@ import { echo } from "@/utils";
 import { globalCss } from "./data";
 import type { SHADCN_THEME } from "@/types";
 
-const dependencies: string[] = ["@heroui/theme", "@heroui/system"];
-const devDependencies: string[] = [];
-
-const command = ["@heroui/button"];
+const dependencies: string[] = [
+  "class-variance-authority",
+  "clsx",
+  "lucide-react",
+  "tailwind-merge",
+];
+const devDependencies: string[] = ["tailwind-merge"];
 
 export async function shadcnSetup(appDir: string, theme: SHADCN_THEME) {
   try {
     await echo(path.join(appDir, "compoenents.json"), globalCss(theme));
-    return { dependencies, devDependencies, command };
+    return { dependencies, devDependencies };
   } catch (err) {
     console.error(err);
     process.exit(1);
