617715f5745799107affa8892ec66830defeea43
SurajKharkwal <kharkwalsuraj13@gmail.com>
2025-08-13 18:07:31 +0530

    Feat : pages-routes

---
+++ base/next/page_routes/extra/ui/shadcn/shadcn.setup.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { echo } from "@/utils";
+import { globalCss } from "./data";
+import type { SHADCN_THEME } from "@/types";
+
+const dependencies: string[] = [
+  "class-variance-authority",
+  "clsx",
+  "lucide-react",
+  "tailwind-merge",
+];
+const devDependencies: string[] = ["tailwind-merge"];
+
+export async function shadcnSetup(appDir: string, theme: SHADCN_THEME) {
+  try {
+    await echo(path.join(appDir, "compoenents.json"), globalCss(theme));
+    return { dependencies, devDependencies };
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
